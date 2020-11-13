430549ec0a16e42e9b3649d1d9fa0f27ca66a82b
Lash-a <64647074+Lash-a@users.noreply.github.com>
2020-11-13 13:37:55 +0400

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,318 @@
+let songs = [
+
+    {
+        title: 'November Rain',
+        author: 'Guns N\' Roses',
+        duration: '8:57',
+        audio: 'tracks/Guns N\' Roses - November Rain LYRICS.mp3',
+        imageUrl: 'https://i2.wp.com/ghostcultmag.com/wp-content/uploads/2015/12/guns-n-roses-classic-logo.jpg?resize=584%2C453&ssl=1'
+    },
+    
+    {
+        title: 'Dont Cry',
+        author: 'Guns N\' Roses',
+        duration: '4:54',
+        audio: 'tracks/Guns N\' Roses - Don\'t Cry with Lyrics, music.mp3',
+        imageUrl: 'https://i2.wp.com/ghostcultmag.com/wp-content/uploads/2015/12/guns-n-roses-classic-logo.jpg?resize=584%2C453&ssl=1'
+    },
+     
+    {
+        title: 'Nothing else matter',
+        author: 'Metallica',
+        duration: '6:36',
+        audio: 'tracks/Metallica - Nothing else matter lyrics.mp3',
+        imageUrl: 'https://i1.sndcdn.com/artworks-000448922046-twktrx-t500x500.jpg'
+    },
+
+    {
+        title: 'I Don\'t Wanna Miss a Thing',
+        author: 'Aerosmith',
+        duration: '4:56',
+        audio: 'tracks/Aerosmith -I Don\'t Wanna Miss a Thing Lyrics.mp3',
+        imageUrl: 'https://i.pinimg.com/originals/22/30/cb/2230cbe6ca2e7796175ec14a27193994.jpg'
+    },
+
+    {
+        title: 'Hotel California',
+        author: 'Eagles',
+        duration: '6:30',
+        audio: 'tracks/Eagles - Hotel California.mp3',
+        imageUrl: 'https://i.pinimg.com/564x/db/6f/12/db6f12132ddcab7f33d448c1b0eb4fdc.jpg'
+    },
+
+    {
+        title: 'High Hopes',
+        author: 'Pink Floyd',
+        duration: '6:59',
+        audio: 'tracks/Pink Floyd - High Hopes [Lyrics].mp3',
+        imageUrl: 'https://i.pinimg.com/originals/c5/fe/53/c5fe5325dc969dcf74620c0c71c16645.jpg'
+    },
+    {
+        title: 'Smells Like Teen Spirit',
+        author: 'Nirvana',
+        duration: '4:38',
+        audio: 'tracks/Nirvana - Smells Like Teen Spirit (Official Music Video).mp3',
+        imageUrl: 'https://i.pinimg.com/originals/f5/9e/41/f59e413d576f99dd7a5f7814e4af91f2.jpg'
+    },
+    {
+        title: 'Highway to Hell',
+        author: 'ACDC',
+        duration: '3:28',
+        audio: 'tracks/ACDC - Highway to Hell.mp3',
+        imageUrl: 'https://images.prismic.io/merchbar/944e8df6a9b461db583b94cbef59215c85a3a56d_167494b31a5019c81c13bb11029352b6.jpg?auto=compress,format'
+    },
+    {
+        title: 'Dream on',
+        author: 'Aerosmith',
+        duration: '4:27',
+        audio: 'tracks/Aerosmith - Dream on- Lyrics.mp3',
+        imageUrl: 'https://i.pinimg.com/originals/22/30/cb/2230cbe6ca2e7796175ec14a27193994.jpg'
+    },
+    {
+        title: 'It\'s my life',
+        author: 'Bon Jovi',
+        duration: '4:01',
+        audio: 'tracks/Bon Jovi - It\'s my life w lyrics.mp3',
+        imageUrl: 'https://i.pinimg.com/originals/5d/69/00/5d6900a4ee1dc5dd32d1469496fdb574.jpg'
+    },
+    {
+        title: 'Soldier of Fortune',
+        author: 'Deep Purple',
+        duration: '3:13',
+        audio: 'tracks/Deep Purple - Soldier of Fortune Lyrics.mp3',
+        imageUrl: 'https://img.artpal.com/38639/259-19-9-2-7-53-7m.jpg'
+    },
+    {
+        title: 'This I Love',
+        author: 'Guns N Roses',
+        duration: '5:34',
+        audio: 'tracks/Guns N Roses - This I Love.mp3',
+        imageUrl: 'https://i2.wp.com/ghostcultmag.com/wp-content/uploads/2015/12/guns-n-roses-classic-logo.jpg?resize=584%2C453&ssl=1'
+    },
+    {
+        title: 'The Show Must Go On',
+        author: 'Queen',
+        duration: '4:07',
+        audio: 'tracks/Queen - The Show Must Go On (with lyrics)In memory of Freddie Mercury.mp3',
+        imageUrl: 'https://i1.sndcdn.com/artworks-000280794164-3lyacz-t500x500.jpg'
+    },
+    {
+        title: 'Snow',
+        author: 'Red Hot Chili Peppers',
+        duration: '5:49',
+        audio: 'tracks/Red Hot Chili Peppers - Snow (Hey Oh) (Official Music Video).mp3',
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51DUnkjmziL._AC_SL1024_.jpg'
+    },
+    {
+        title: 'Still Loving You',
+        author: 'Scorpions',
+        duration: '6:26',
+        audio: 'tracks/Scorpions - Still Loving You ( lyrics ).mp3',
+        imageUrl: 'https://i.pinimg.com/originals/77/ad/7a/77ad7aa5f7eadfa38c9d363a8f4c1dad.jpg'
+    },
+    {
+        title: 'The Final Countdown',
+        author: 'Europe',
+        duration: '5:12',
+        audio: 'tracks/The Final Countdown - Europe (Lyrics).mp3',
+        imageUrl: 'https://sd.keepcalms.com/i-w600/keep-calm-it-s-the-final-countdown-69.jpg'
+    },
+    {
+        title: 'Eye Of The Tiger',
+        author: 'Survivor',
+        duration: '4:02',
+        audio: 'tracks/Survivor - Eye of the Tiger (Lyrics).mp3',
+        imageUrl: 'https://img.discogs.com/AwAapcAANjM1S8MevBpSq_sphcM=/fit-in/600x611/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1164120-1239890207.jpeg.jpg'
+    },
+    {
+        title: 'Lady in black',
+        author: 'Uriah Heep',
+        duration: '4:50',
+        audio: 'tracks/Uriah Heep - Lady in black.mp3',
+        imageUrl: 'https://whiplash.net/imagens_promo/uriahheep_logo.jpg'
+    },
+    {
+        title: 'Californication',
+        author: 'Red Hot Chili Peppers',
+        duration: '5:42',
+        audio: 'tracks/Red Hot Chili PeppersCalifornication [Official Video].mp3',
+        imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51DUnkjmziL._AC_SL1024_.jpg'
+    }
+
+];
+
+let artistImage = document.getElementById('artist-image');
+let artist = document.getElementById('song-author');
+let title = document.getElementById('song-title');
+let playBtn = document.getElementById('play-btn');
+let audioElem = document.getElementById('myAudio');
+let forwardBtn = document.getElementById('forward-btn');
+let backWardBtn = document.getElementById('backward-btn');
+let songPlayer = document.getElementById('song-player');
+let endingTime = document.getElementById('ending-time');
+let next = document.getElementById('next');
+let pervious = document.getElementById('pervious');
+let playNextOrLoop = document.querySelector('.loop');
+let shuffleButton = document.querySelector('.shuffle');
+let songCounter = 0;
+let shuffle = false;
+let loopOrNext = true;
+
+const changeContent = () => {
+    artistImage.src = songs[songCounter].imageUrl;
+    artist.innerHTML = songs[songCounter].author;
+    title.innerHTML = songs[songCounter].title;
+    endingTime.innerHTML = songs[songCounter].duration;
+};
+changeContent();
+
+
+// თუ დაწყებულია სიმღერა ცვლის პაუზის იკონს 
+const playAndStopBtn = () => {
+    let checkIfPlaying = false;
+
+    playBtn.addEventListener('click', () => {
+        if(checkIfPlaying == false){
+            // ამით იცვლება აუდიო ობიექტიდან
+            songPlayer.src = songs[songCounter].audio;
+            audioElem.play();
+            playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+            checkIfPlaying = true;
+        }else{
+            playBtn.innerHTML = '<i class="fas fa-play"></i>';
+            audioElem.pause();
+            checkIfPlaying = false;
+        }
+    });
+}
+playAndStopBtn();
+
+// ცვლის სიმღერებს
+const checkNextOrPervious = () => {
+
+    next.addEventListener('click', () => {
+        if(songCounter != songs.length - 1){
+            if(songCounter < songs.length - 1){
+                songCounter++;
+                changeContent();
+                play();
+                playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+            }
+        }else{
+            songCounter = 0;
+            changeContent();
+            play();
+        }
+      
+    });
+
+    pervious.addEventListener('click', () => {
+        if(songCounter != 0){
+            if(songCounter > 0 && songCounter <= songs.length){
+                songCounter--;
+                changeContent();
+                play();
+                playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+            } 
+        }else {
+            songCounter = songs.length - 1;
+            playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+            changeContent();
+            play();
+        }
+       
+    });
+};
+checkNextOrPervious();
+
+// გამოიყენება სხვა ფუნქციებში კოდის დასაპატარავებლად
+// ცვლის src-ს და მერე ანახლებს აუდიო ელემენტის ლინკს
+const play = () => {
+    songPlayer.src = songs[songCounter].audio;
+    audioElem.load();
+    audioElem.play();
+};
+
+
+
+// გადაახვევს სიმღერას 10 წამით წინ
+const forwardTenSeconds = () => {
+    forwardBtn.addEventListener('click', () =>{
+        audioElem.currentTime += 10;
+    });
+}
+forwardTenSeconds();
+
+// გადაახვევს სიმღერას 10 წამით უკან
+const backwardsTenSeconds = () => {
+    backWardBtn.addEventListener('click', () =>{
+        audioElem.currentTime -= 10;
+    });
+}
+backwardsTenSeconds();
+
+// shuffle ცვლადს მიანიჭებს true ან false მნიშვნელობას
+// counter ცვლადი დამატებულია იმისთვის რომ 
+// დაჭერისას გავიგოთ false იყო მნიშვნელობა თუ არა
+const shuffleFunc = () => {
+    let counter = 10;
+    shuffleButton.addEventListener('click', () => {
+        if(counter % 2 === 0){
+            shuffleButton.style.opacity = '1';
+            shuffle = true;
+            counter--;
+        }else {
+            shuffle = false;
+            counter++;
+            shuffleButton.style.opacity = '0.6';
+        }
+    });
+} 
+shuffleFunc();
+
+// audioElem.onended ეს ფუნქცია მუშაობს მხოლოდ მაშინ როცა სიმღერა მორჩება 
+// შემდეგ shuffle ცვლადის დახმარებით ვამოწმებთ დაჭერია თუ არა 
+// shuffle ბრძანებაზე და ამის შემდეგ songCounter ცვლადი ხდება random რიცხვი
+// 0-დან songs.objectis სიგრძემდე ნებისმიერი რიცხვი
+const checkShuffleOrNot = () => {
+    audioElem.onended = () => {
+        if(shuffle){
+            let randomSong = Math.floor(Math.random() * songs.length);
+            songCounter = randomSong;
+            if(loopOrNext){
+                changeContent();
+                play();
+            }else if(loopOrNext === false) {
+                play();
+            }
+        }else{
+            if(loopOrNext){
+                songCounter++;
+                changeContent();
+                play();
+            }else if(loopOrNext === false) {
+                play();
+            }
+        }
+    }
+};
+checkShuffleOrNot();
+
+// ცვლის სიმღერის გამეორების ფუნქციონალს შემდეგ 
+// სიმღერაზე გადასვლის ფუნქციონალზე
+const checkLoopOrNext = () => {
+    let counter = 0;
+    playNextOrLoop.addEventListener('click',() =>{
+        if(counter == 0){
+            playNextOrLoop.innerHTML = '<i class="fas fa-recycle"></i>';
+            counter++;
+            loopOrNext = false;
+            shuffle = false;
+        }else {
+            counter--;
+            playNextOrLoop.innerHTML = '<i class="fas fa-long-arrow-alt-right"></i>';
+            loopOrNext = true;
+        }
+    });
+};
+checkLoopOrNext();
